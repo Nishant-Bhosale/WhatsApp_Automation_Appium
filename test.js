cd3c1e3ff05279e52824bd0cc163f913e6bd55c6
Nishant-Bhosale <nishantbhosale244@gmail.com>
2023-09-29 17:44:55 +0530

    feat: sending images

--- test.js
+++ test.js
@@ -18,43 +18,27 @@ const wdOpts = {
 
 let driver;
 
-// const tests = [
-//   {
-//     type: "message",
-//     message: "Hii, this message is sent to you using appium!",
-//     contact: "Anudip Gupta",
-//   },
-// ];
+// gallery attach id - com.whatsapp:id/pickfiletype_gallery_holder
 
 async function runTest() {
   driver = await remote(wdOpts);
 
-  await searchUserAndSendMessage(
-    "Anudip Gupta",
-    "Hii, this message is sent to you using appium!"
-  );
+  try {
+    // Normal Message
+    // await searchUserAndSendMessage(
+    //   "Anudip Gupta",
+    //   "Hii, this message is sent to you using appium!"
+    // );
 
-  await goBack();
-
-  await driver.pause(1000);
-  await driver.deleteSession();
-
-  // tests.forEach(async (test) => {
-  //   const { type, message, contact } = test;
-
-  // try {
-  //   if (type === "message") {
-  //     await searchUserAndSendMessage(contact, message);
-  //     await goBack();
-  //   } else if (type === "image") {
-  //     console.log("fsd");
-  //   } else {
-  //     console.log("Fdsf");
-  //   }
-  // } catch (error) {
-  //   console.log("Error:", error);
-  // }
-  // });
+    // Sending Image
+    await searchUserAndSendImage("Anudip Gupta", "IMG_20210215_173648.jpg");
+    await goBack();
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await driver.pause(1000);
+    await driver.deleteSession();
+  }
 }
 
 const selectElementById = async (id) => {
@@ -62,6 +46,65 @@ const selectElementById = async (id) => {
 };
 
 const searchUserAndSendMessage = async (userName, message) => {
+  await searchAndSelectUser(userName);
+
+  await selectAndFillInput(message);
+
+  await clickSendButton();
+};
+
+const searchUserAndSendImage = async (userName, imageName) => {
+  await searchAndSelectUser(userName);
+
+  const attachButton = await selectElementById(
+    "com.whatsapp:id/input_attach_button"
+  );
+
+  await attachButton.click();
+
+  const attachDocumentButton = await selectElementById(
+    "com.whatsapp:id/pickfiletype_document_holder"
+  );
+
+  await attachDocumentButton.click();
+
+  const browseOtherDocsButton = await selectElementById(
+    "com.whatsapp:id/browseOtherDocs"
+  );
+
+  await browseOtherDocsButton.click();
+
+  const imagesTab = await driver.$('//*[@text="Images"]');
+
+  await imagesTab.click();
+
+  const searchButton = await selectElementById(
+    "com.google.android.documentsui:id/option_menu_search"
+  );
+
+  await searchButton.click();
+
+  const searchInput = await selectElementById(
+    "com.google.android.documentsui:id/search_src_text"
+  );
+
+  await searchInput.click();
+
+  await searchInput.setValue(imageName);
+  await driver.hideKeyboard();
+
+  await driver.pause(15000);
+
+  const image = await driver.$(
+    `android= new UiSelector().resourceId("android:id/title").text("${imageName}")`
+  );
+
+  await image.click();
+
+  await clickSendButton();
+};
+
+const searchAndSelectUser = async (userName) => {
   const searchButton = await selectElementById(
     "com.whatsapp:id/menuitem_search"
   );
@@ -69,9 +112,7 @@ const searchUserAndSendMessage = async (userName, message) => {
   await searchButton.click();
 
   const searchInput = await selectElementById("com.whatsapp:id/search_input");
-
   await searchInput.click();
-
   await searchInput.setValue(userName);
 
   await driver.hideKeyboard();
@@ -81,18 +122,22 @@ const searchUserAndSendMessage = async (userName, message) => {
   );
 
   await contact.click();
-
-  const messageInput = await selectElementById("com.whatsapp:id/entry");
-  await messageInput.click();
-  await messageInput.setValue(message);
-
-  const sendMessageButton = await selectElementById("com.whatsapp:id/send");
-  await sendMessageButton.click();
 };
 
 const goBack = async () => {
   const backButton = await selectElementById("com.whatsapp:id/back");
   await backButton.click();
+};
+
+const clickSendButton = async () => {
+  const sendMessageButton = await selectElementById("com.whatsapp:id/send");
+  await sendMessageButton.click();
+};
+
+const selectAndFillInput = async (message) => {
+  const messageInput = await selectElementById("com.whatsapp:id/entry");
+  await messageInput.click();
+  await messageInput.setValue(message);
 };
 
 runTest().catch(console.error);
